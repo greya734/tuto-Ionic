3fa9442b9523c0dc07cd1c8d062d7746bd6b95a1
greya734 <rosajoshu@gmail.com>
2026-07-02 11:52:37 +1100

    fix app

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+
+defineCustomElements(window);
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -11,4 +14,4 @@ bootstrapApplication(AppComponent, {
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
   ],
-});
+}).catch((err) => console.log(err));
