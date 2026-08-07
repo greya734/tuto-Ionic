e79aafead471cfb2abfcdf541cca60c599a0f343
greya734 <rosajoshu@gmail.com>
2026-08-07 15:05:58 +1100

    hotfix: boutton tab error

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Injectable({
@@ -15,5 +14,4 @@ export class PhotoService {
       quality: 100,
     });
   }
-
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon,  IonFab } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { PhotoService } from '../services/photo.service';
 import { addIcons } from 'ionicons';
@@ -9,12 +9,16 @@ import { camera } from 'ionicons/icons';
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, ExploreContainerComponent,  IonFab]
 })
 export class Tab2Page {
 
-  constructor() {
+  constructor(public photoService: PhotoService) {
     addIcons({ camera });
+  }
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
   }
 
 }
